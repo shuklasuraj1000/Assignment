2606809b644b3780fe4073c4e3b7ca7d41c0b488
SOORAJ SHUKLA <105222437+shuklasuraj1000@users.noreply.github.com>
2022-06-12 11:43:33 +0530

    Created using Colaboratory

---
+++ test.py
@@ -0,0 +1,44 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled60.ipynb",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyNFESObQz8+22q9rFUc4mW0",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/shuklasuraj1000/Assignment/blob/main/test.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "def addition1(a,b):\n",
+        "  return a+b"
+      ],
+      "metadata": {
+        "id": "dNTrjsnriEmQ"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
